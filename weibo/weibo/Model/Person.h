cff404bfbe78b6d987b048a93178f4680d4efb35
HCL黄 <hclhuang@hclhuangdemacbook-pro.local>
2016-10-31 13:31:17 +0800

    创建了Person类

---
+++ weibo/weibo/Model/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  weibo
+//
+//  Created by HCL黄 on 16/10/31.
+//  Copyright © 2016年 HCL黄. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@end
---
+++ weibo/weibo/Model/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  weibo
+//
+//  Created by HCL黄 on 16/10/31.
+//  Copyright © 2016年 HCL黄. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
